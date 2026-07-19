377331090acf4d436acc9faf34dadf1e87d8df10
linuseklof1-maker <linuseklof1@gmail.com>
2026-07-19 18:10:16 +0300

    Sluta spåra node_modules och _site

--- _site/js/meny.js
+++
@@ -1,9 +0,0 @@
-const menyKnapp = document.querySelector(".meny-knapp");
-const huvudMeny = document.querySelector(".huvudmeny");
-
-if (menyKnapp && huvudMeny) {
-  menyKnapp.addEventListener("click", function () {
-    const oppen = huvudMeny.classList.toggle("oppen");
-    menyKnapp.setAttribute("aria-expanded", oppen);
-  });
-}
