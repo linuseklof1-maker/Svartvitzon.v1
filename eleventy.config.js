d66de54dd8fe8af41dec5008057e32071f7e7179
linuseklof1-maker <linuseklof1@gmail.com>
2026-07-20 19:41:43 +0300

    Passthrough för admin-mappen

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,3 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bilder");
   eleventyConfig.addPassthroughCopy("data");
 };
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("bilder");
+  eleventyConfig.addPassthroughCopy("data");
+  eleventyConfig.addPassthroughCopy("admin");
+};
